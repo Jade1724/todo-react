607c6f5d3031f46ae5c3e1e44069c38127344e5c
Jade1724 <haruka.i1724@gmail.com>
2023-02-22 15:41:34 +1300

    Used PropTypes to specify string type on name props.

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,7 +1,9 @@
 import React, { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
 
-export const Profile: FC = (): ReactElement => {
+export const Profile: FC = (props: any): ReactElement => {
+  const { name = 'John' } = props;
   return (
     <Box
       display="flex"
@@ -18,15 +20,19 @@ export const Profile: FC = (): ReactElement => {
         }}
       >
         <Typography variant="h4" color="text.primary">
-          J
+          {`${name.substring(0, 1)}`}
         </Typography>
       </Avatar>
       <Typography variant="h6" color="text.primary">
-        Welcom, John
+        {`Welcome, ${name}`}
       </Typography>
       <Typography variant="body1" color="text.primary">
         This is your personal tasks manager
       </Typography>
     </Box>
   );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string,
 };
